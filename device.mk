a297fcf8ec7f039c54430cbe7d04d2c496d21063
ragy365 <wad.moskla@gmail.com>
2023-01-14 04:39:45 +0530

    surya: Include MIUI Camera

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, vendor/xiaomi/surya/surya-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, vendor/xiaomi/surya-miuicamera/products/miuicamera.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
